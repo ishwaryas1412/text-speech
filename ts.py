cacc1f3a80104dbdd90c9ce9fade07f8b48c28ff
Ishwarya Sekar <52875849+ishwaryas1412@users.noreply.github.com>
2020-07-29 10:01:47 +0530

    text to speech

---
+++ ts.py
@@ -0,0 +1,10 @@
+import pyttsx3
+
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+engine.say("welcome")
+engine.say("I am Ishwarya sekar")
+engine.say("python developer")
+engine.runAndWait()
